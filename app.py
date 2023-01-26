66c025f4e2303d3aabf53c3725ed2707027232f5
Rory <rorywbush@yahoo.co.uk>
2023-01-26 11:24:03 +0000

    add route in app

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from authentication import authentication
 from portfolio import portfolio
+from chartData import chartData
 
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(portfolio, url_prefix="")
 app.register_blueprint(authentication, url_prefix="")
+app.register_blueprint(chartData, url_prefix="")
+
 
 app.secret_key = "secret"
 
