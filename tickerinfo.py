1e03e45aeb1aa7de6b935ee9d7155c1f97acd67e
yusuf <majorhaji@github.com>
2023-01-28 04:10:03 +0000

    changed info arr to info dictionary

--- tickerinfo.py
+++ tickerinfo.py
@@ -4,16 +4,17 @@ import pyrebase
 import json
 tickerinfo = Blueprint('tickerinfo', __name__)
 
+#/api/tickerinfo?tickerArr=AAPL&tickerArr=GOOG
 
 @tickerinfo.route(f"/api/tickerinfo", methods=["GET"])
 def get_price():
     ticker_arr = request.args.getlist("tickerArr")
     if not ticker_arr:
         return "No tickerArr parameter in the request", 400
-    info_arr = []
+    info_dict = {}
     for ticker in ticker_arr:
         stock = yf.Ticker(ticker)
         price = stock.fast_info['last_price']
-        info_arr.append({ticker: price})
-    return info_arr
+        info_dict[ticker] = price
+    return info_dict
   
