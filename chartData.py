099309ef421fec015e005a2f0904a242e505a74e
Rory <rorywbush@yahoo.co.uk>
2023-01-30 12:16:01 +0000

    stock is now dynamic and changes depending on the page the user is viewing

--- chartData.py
+++ chartData.py
@@ -6,18 +6,13 @@ import pandas as pd
 chartData = Blueprint('stock', __name__)
 
 
-stock = yf.Ticker('tsla')
-stockHistory = stock.history(period="max")
-stockHistory = stockHistory.reset_index()
-del stockHistory['Dividends']
-del stockHistory['Stock Splits']
-
-stockHistory['Date'] = pd.to_datetime(stockHistory["Date"].dt.strftime('%Y-%m-%d'))
-
-
-stockJson = stockHistory.to_json(date_format='iso')
-
-
-@chartData.route('/stock')
-def chart_data():
+@chartData.route('/stock/<stockname>')
+def chart_data(stockname):
+    stock = yf.Ticker(stockname)
+    stockHistory = stock.history(period="max")
+    stockHistory = stockHistory.reset_index()
+    del stockHistory['Dividends']
+    del stockHistory['Stock Splits']
+    stockHistory['Date'] = pd.to_datetime(stockHistory["Date"].dt.strftime('%Y-%m-%d'))
+    stockJson = stockHistory.to_json(date_format='iso')
     return {'stock': stockJson}
