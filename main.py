6ca6a2b728edf665d35f870c1b48375e61c9bae1
Rory <rorywbush@yahoo.co.uk>
2023-01-31 21:29:54 +0000

    change app.py to main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_cors import CORS
+from portfolio import portfolio
+from chartData import chartData
+from stocknews import stocknews
+from stockData import stockData
+from stockList import stockList
+from tickerinfo import tickerinfo
+from events import calendar
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(portfolio, url_prefix="")
+app.register_blueprint(chartData, url_prefix="")
+app.register_blueprint(stocknews, url_prefix="")
+app.register_blueprint(stockData, url_prefix="")
+app.register_blueprint(stockList, url_prefix="")
+app.register_blueprint(tickerinfo, url_prefix="")
+app.register_blueprint(calendar, url_prefix="")
+
+
+if __name__ == "__main__":
+    app.run()
