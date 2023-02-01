f1db97577502bc0a89f7a0d4501d93f24a9242dd
Rory <rorywbush@yahoo.co.uk>
2023-02-01 19:35:24 +0000

    finish work on new machine learning model using linear regression instead of forest, can now predict the next 5 days

--- stockai.py
+++ stockai.py
@@ -3,6 +3,20 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from flask import Flask, request, Blueprint
 from sklearn.metrics import precision_score
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import datetime
+
+
 
 stockai = Blueprint('stockai', __name__)
 
@@ -25,16 +39,29 @@ def backtest(data, model, predictors, start=2016, step=252):
         all_predictions.append(predictions)
     return pd.concat(all_predictions)
 
+
+
+# stockPredictions['Date'] = pd.to_datetime(stockPredictions["Date"].dt.strftime('%Y-%m-%d'))
+
+
+
+# model = RandomForestClassifier(n_estimators=200, min_samples_split=50, random_state=1)
+# stockPredictions = backtest(sh, model, stock_predictors)
+# modelPredictions = stockPredictions['Predictions'] == 1
+# daysWhereModelPredicts = stockPredictions[modelPredictions]
+# daysWhereModelPredicts = daysWhereModelPredicts.tail(10)
+
 @stockai.route('/api/stockai/<stockname>')
 def stock_ai(stockname):
     stock = yf.Ticker(stockname)
+    model = LinearRegression(n_jobs=-1)
     sh = stock.history(period='max')
     del sh['Dividends']
     del sh['Stock Splits']
     sh['Tomorrow'] = sh['Close'].shift(-1)
     sh['Target'] = (sh['Tomorrow'] > sh['Close']).astype(int)
-    sh = sh.loc['1995-01-01':].copy()
-    time_horizons = [1, 2, 5, 30, 63, 126, 252, 378, 504, 1008]
+    sh = sh.loc['2000-01-01':].copy()
+    time_horizons = [2, 5, 10, 21, 252, 378, 504, 1008]
     stock_predictors = []
     for horizon in time_horizons:
         rolling_averages = sh.rolling(horizon).mean()
@@ -47,11 +74,30 @@ def stock_ai(stockname):
 
         stock_predictors += [ratio_column, trend_column]
     sh = sh.dropna()
-    model = RandomForestClassifier(n_estimators=200, min_samples_split=50, random_state=1)
-    stockPredictions = backtest(sh, model, stock_predictors)
-    modelPredictions = stockPredictions['Predictions'] == 1
-    daysWhereModelPredicts = stockPredictions[modelPredictions]
-    daysWhereModelPredicts = daysWhereModelPredicts.drop(columns=['Target'])
-    print(daysWhereModelPredicts)
-    jsonModel = daysWhereModelPredicts.to_json()
+    forecastOnePercent = int(math.ceil(0.01 * len(sh)))
+    sh['label'] = sh['Tomorrow'].shift(-forecastOnePercent)
+    X = np.array(sh.drop(['Tomorrow'], 1))
+    X = preprocessing.scale(X)
+    X_lately = X[-forecastOnePercent:]
+    X = X[:-forecastOnePercent]
+    X_lately = X[-forecastOnePercent:]
+    y = np.array(sh['label'])
+    y = y[:-forecastOnePercent]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    model.fit(X_train, y_train)
+    forecast_set = model.predict(X_lately)
+    sh['Forecast'] = np.nan
+    last_date = sh.iloc[-1].name
+    last_unix = last_date
+    next_unix = last_unix + datetime.timedelta(days=1)
+    for i in forecast_set:
+        next_date = next_unix
+        next_unix += datetime.timedelta(days=1)
+        sh.loc[next_date] = [np.nan for _ in range(len(sh.columns)-1)]+[i]
+    sh = sh.reset_index()
+    stockPredictions = sh[['Date','Forecast']]
+    today = datetime.datetime.today().date()
+    stockPredictions['Date'] = stockPredictions['Date'].dt.date
+    stockPredictions5Days = stockPredictions[(stockPredictions['Date'] > today) & (stockPredictions['Date'] <= today + pd.Timedelta(days=5))]
+    jsonModel = stockPredictions5Days.to_json()
     return jsonModel
